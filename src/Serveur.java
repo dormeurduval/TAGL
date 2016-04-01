1b17f540c560f5584a3ec547e27b0db6986d21bb
chaffarj <chaffard.joris@outlook.fr>
2016-04-01 12:05:47 +0200

    "ajout de Client"

--- src/Serveur.java
+++ src/Serveur.java
@@ -1,4 +1,16 @@
+import java.util.*;
 
 public class Serveur {
-
+	Map<Integer ,Object> table;
+	
+	public void ajouterString(Object s){
+		table.put(s.hashCode(),s);
+	}
+	
+	public Object trouverObject(Integer k){
+		return table.get(k);
+	}
+	
+	
+	
 }
