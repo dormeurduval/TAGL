b21aa7b5e55a24666fbf05d54c3271d0638fec49
chaffarj <chaffard.joris@outlook.fr>
2016-04-08 10:40:53 +0200

    "ajout de maven"

--- src/Serveur.java
+++ src/Serveur.java
@@ -1,10 +1,27 @@
 import java.util.*;
 
 public class Serveur {
+	int taille_courante;
+	int taille_max;
+	Object last_object;
+	
 	Map<Integer ,Object> table;
 	
-	public void ajouterString(Object s){
+	public void Serveur(int taille_max){
+		taille_courante = 0;
+		this.taille_max = taille_max;
+		table = new HashMap<Integer ,Object>();
+		
+	}
+	
+	public void ajouterObject(Object s){
+		if(taille_courante == taille_max)
+			table.remove(last_object);
+		else
+			taille_courante++;
+		
 		table.put(s.hashCode(),s);
+		last_object = s;
 	}
 	
 	public Object trouverObject(Integer k){
